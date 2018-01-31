2dc6fb66340f7715920392d22a5ede61900bacc3
Peter J. Holzer <hjp@wsr.ac.at>
2018-01-31 10:42:48 +0100

    Move hardcoded URL to environment

--- 04 Routing/VouchersRouting/src/environments/environment.prod.ts
+++ 04 Routing/VouchersRouting/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  authEnabled: true,
+  vouchersAPI: '/assets/vouchers.json'
 };
--- 04 Routing/VouchersRouting/src/environments/environment.ts
+++ 04 Routing/VouchersRouting/src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
     production: false,
-    authEnabled: false
+    authEnabled: true,
+    vouchersAPI: '/assets/vouchers.json'
 };
